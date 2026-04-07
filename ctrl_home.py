860074fbeb6f42c79deda0f914060da2ce143584
ALEXANDRE ROBERTO DA CRUZ SILVA <alexandre.cruz@portalsesisp.org.br>
2026-04-07 09:38:12 -0300

    template dashboard

--- ctrl_home.py
+++ ctrl_home.py
@@ -16,5 +16,12 @@ def index(): # função que gerencia rota
     if 'user' not in session:
         return redirect(url_for("auth.login"))
     
-    return render_template("index.html") # Renderiza um template
-    
+    return render_template("dashboard/index.html") # Renderiza um template
+    ks
+
+@bp.route("/dashboard") # cria uma rota
+def dashboard(): # função que gerencia rota
+    """ Painel de vendas"""
+    # if 'user' not in session:
+        # return redirect(url_for("auth.login"))
+    return render_template("dashboard/index.html", title="Painel de vendas") # Renderiza um template
