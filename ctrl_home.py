2dca894f36dd76ad8e643d6b054d07c172efa810
ALEXANDRE ROBERTO DA CRUZ SILVA <alexandre.cruz@portalsesisp.org.br>
2026-05-12 07:37:56 -0300

    aula 06

--- ctrl_home.py
+++ ctrl_home.py
@@ -23,5 +23,27 @@ def dashboard(): # função que gerencia rota deve ser única
     """ Painel de Vendas"""
     #  if 'user' not in session:  # garnate autenticação
     #       return redirect(url_for("auth.login"))
-    
-    return render_template("dashboard/index.html", title = "Painel de Vendas") # Renderiza um template
+
+    import locale
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+    vendas: list = [
+        {"mês": "Janeiro" , "total" :139519.19},
+        {"mês": "fevereiro" , "total" :99519.19},
+        {"mês": "março" , "total" :149519.19},
+        {"mês": "abril" , "total" :179519.19},
+        {"mês": "maio" , "total" :199519.19},
+        {"mês": "junho" , "total" :259519.19},
+        {"mês": "julho" , "total" :509519.19},
+        {"mês": "agosto" , "total" :209519.19},
+        {"mês": "setembro" , "total" :129519.19},
+        {"mês": "outubro" , "total" :119519.19},
+        {"mês": "novembro" , "total" :109519.19},
+        {"mês": "dezembrO" , "total" :479519.19},
+
+
+     
+    ] # fim lista de vendas
+    return render_template("dashboard/index.html", title = "Painel de Vendas", vendas = vendas, locale = locale ) 
+# Renderiza um template
+
